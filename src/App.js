67d0e0770fd8df46e7022832c7bd5e21a6cc7299
ericchi00 <ericchi00@gmail.com>
2022-04-18 13:05:55 -0700

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import SubredditList from './components/SubredditList';
+import './style/index.css';
 
 const App = () => {
-	return;
+	// const [subreddit, setSubreddit] = useState([]);
+
+	return (
+		<BrowserRouter>
+			<Header />
+			<SubredditList />
+			<Routes>
+				<Route path="/" element="" />
+				<Route path="/" element="" />
+				<Route path="/" element="" />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;

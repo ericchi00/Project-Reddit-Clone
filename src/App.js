0734994aae7691644676a6194e25130f14d9f6b3
ericchi00 <ericchi00@gmail.com>
2022-04-19 17:55:48 -0700

    clarified title & changed route element

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import SubredditList from './components/SubredditList';
 import Subreddit from './components/Subreddit';
 import Post from './components/Post';
+import Comment from './components/Comment';
 import './style/index.css';
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
 			<Routes>
 				<Route path="/" element="" />
 				<Route path="/r/:subreddit" element={<Subreddit />} />
-				<Route path="/r/:subreddit/:title" element={<Post />} />
+				<Route path="/r/:subreddit/:postTitle" element={<Comment />} />
 			</Routes>
 		</BrowserRouter>
 	);

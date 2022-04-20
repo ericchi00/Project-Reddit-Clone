030c9bdff46186b72950351e0a7102619d9e12dc
ericchi00 <ericchi00@gmail.com>
2022-04-20 12:36:10 -0700

    added sign in wtih google auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {
+	getAuth,
+	GoogleAuthProvider,
+	signInWithPopup,
+	onAuthStateChanged,
+	signOut,
+} from 'firebase/auth';
 import bot from '../images/bot.svg';
 
 const Header = () => {
+	const [signedIn, setSignedIn] = useState(false);
+	const [username, setUserName] = useState('');
+	const provider = new GoogleAuthProvider();
+	const auth = getAuth();
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setUserName(user.displayName);
+				setSignedIn(true);
+			} else {
+				return;
+			}
+		});
+	});
+
+	const login = () => {
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				const token = credential.accessToken;
+				const user = result.user;
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				const email = error.email;
+				const credential = GoogleAuthProvider.credentialFromError(error);
+				console.log(errorCode, errorMessage, email, credential);
+			});
+	};
+
+	const signout = () => {
+		signOut(auth)
+			.then(() => {
+				setSignedIn(false);
+			})
+			.catch((error) => {
+				alert(error, 'Please try again!');
+			});
+	};
+
 	return (
 		<header>
 			<nav>
@@ -14,12 +63,30 @@ const Header = () => {
 						<li className="link-header">Hot</li>
 						<li className="link-header">New</li>
 					</div>
-					<div className="google-login">
-						<p>
-							Google Sign(this will display user name or it will display sign
-							in)
-						</p>
-					</div>
+					{!signedIn ? (
+						<div className="google-wrapper">
+							<button
+								type="button"
+								className="google-login"
+								onClick={() => login()}
+							>
+								Login with Google
+							</button>
+						</div>
+					) : (
+						<div className="google-signedin">
+							<div className="google-username">
+								Signed in as: <span>{username}</span>
+							</div>
+							<button
+								type="button"
+								className="signout"
+								onClick={() => signout()}
+							>
+								Sign Out
+							</button>
+						</div>
+					)}
 				</ul>
 			</nav>
 		</header>

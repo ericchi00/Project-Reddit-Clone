4dc4146c3db8f99826d037285dada55075cbbac9
ericchi00 <ericchi00@gmail.com>
2022-04-26 12:55:50 -0700

    removed commented code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import SubredditList from './components/SubredditList';
 import Subreddit from './components/Subreddit';
 import Comment from './components/Comment';
+import Homepage from './components/Homepage';
 import {
 	getAuth,
 	GoogleAuthProvider,
@@ -37,21 +38,6 @@ const App = () => {
 		});
 	});
 
-	// const onSubmit = async (e) => {
-	// 	e.preventDefault();
-	// 	const db = getFirestore();
-	// 	const checkIfSubredditExists = doc(db, 'Subreddit', name);
-	// 	const subreddit = await getDoc(checkIfSubredditExists);
-	// 	if (!subreddit.exists()) {
-	// 		await setDoc(doc(db, 'Subreddit', name), { name: name });
-	// 		setCreate(false);
-	// 	} else {
-	// 		const create = document.getElementsByName('create')[0];
-	// 		create.value = '';
-	// 		create.placeholder = 'Subreddit already exists';
-	// 	}
-	// };
-
 	const login = () => {
 		signInWithPopup(auth, provider).catch((error) => {
 			const errorCode = error.code;
@@ -82,7 +68,7 @@ const App = () => {
 			/>
 			<SubredditList />
 			<Routes>
-				<Route path="/" element="" />
+				<Route path="/" element={<Homepage />} />
 				<Route
 					path="/r/:subreddit"
 					element={<Subreddit username={username} signedIn={signedIn} />}

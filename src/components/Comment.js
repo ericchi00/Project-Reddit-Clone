83976979486b497c5422547e7d346149dcf0aa5d
ericchi00 <ericchi00@gmail.com>
2022-04-27 16:19:18 -0700

    can upvote/downvote

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,20 +1,160 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import up from '../images/arrow-single-up.svg';
 import down from '../images/arrow-single-down.svg';
 import { formatDistanceToNow } from 'date-fns';
+import {
+	collection,
+	doc,
+	getDocs,
+	getFirestore,
+	query,
+	updateDoc,
+	arrayRemove,
+	arrayUnion,
+	increment,
+	getDoc,
+	addDoc,
+	orderBy,
+} from '@firebase/firestore';
 
-const Comment = ({ name, score, text, time }) => {
-	
+const Comment = ({
+	name,
+	score,
+	text,
+	time,
+	signedIn,
+	currentUser,
+	subreddit,
+	postID,
+}) => {
+	useEffect(() => {}, []);
+	const [updatedScore, setUpdatedScore] = useState(score);
+
+	const firestore = getFirestore();
+
+	const upvote = async () => {
+		if (!signedIn) {
+			alert('You must be signed in to vote.');
+			return;
+		}
+		const docRef = doc(firestore, 'UserLikes', currentUser);
+		const docSnap = await getDoc(docRef);
+		const upvote = docSnap.data().upvotes;
+
+		const collectionRef = collection(
+			firestore,
+			`Subreddit/${subreddit}/posts/${postID}/comments`
+		);
+		const q = query(collectionRef);
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach(async (item) => {
+			// searches if user already downvoted & removes it
+			if (upvote.length > 0) {
+				const originalVote = upvote.filter((item) => item === time);
+				if (item.data().timestamp === originalVote[0]) {
+					await updateDoc(
+						doc(
+							firestore,
+							`Subreddit/${subreddit}/posts/${postID}/comments/${item.id}`
+						),
+						{
+							score: increment(-1),
+						}
+					);
+					await updateDoc(doc(firestore, `UserLikes/${currentUser}`), {
+						upvotes: arrayRemove(time),
+					});
+					setUpdatedScore(item.data().score - 1);
+					return;
+				}
+			}
+			// downvotes if user already hasn't downvoted
+			if (item.data().timestamp === time) {
+				await updateDoc(
+					doc(
+						firestore,
+						`Subreddit/${subreddit}/posts/${postID}/comments/${item.id}`
+					),
+					{
+						score: increment(1),
+					}
+				);
+				await updateDoc(doc(firestore, `UserLikes/${currentUser}`), {
+					upvotes: arrayUnion(time),
+					downvotes: arrayRemove(time),
+				});
+				setUpdatedScore(item.data().score + 1);
+			}
+		});
+	};
+
+	const downvote = async () => {
+		if (!signedIn) {
+			alert('You must be signed in to vote.');
+			return;
+		}
+
+		const docRef = doc(firestore, 'UserLikes', currentUser);
+		const docSnap = await getDoc(docRef);
+		const downvote = docSnap.data().downvotes;
+
+		const collectionRef = collection(
+			firestore,
+			`Subreddit/${subreddit}/posts/${postID}/comments`
+		);
+		const q = query(collectionRef);
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach(async (item) => {
+			// searches if user already downvoted & removes it
+			if (downvote.length > 0) {
+				const originalVote = downvote.filter((item) => item === time);
+				if (item.data().timestamp === originalVote[0]) {
+					await updateDoc(
+						doc(
+							firestore,
+							`Subreddit/${subreddit}/posts/${postID}/comments/${item.id}`
+						),
+						{
+							score: increment(1),
+						}
+					);
+					await updateDoc(doc(firestore, `UserLikes/${currentUser}`), {
+						downvotes: arrayRemove(time),
+					});
+					setUpdatedScore(item.data().score + 1);
+					return;
+				}
+			}
+			// downvotes if user already hasn't downvoted
+			if (item.data().timestamp === time) {
+				await updateDoc(
+					doc(
+						firestore,
+						`Subreddit/${subreddit}/posts/${postID}/comments/${item.id}`
+					),
+					{
+						score: increment(-1),
+					}
+				);
+				await updateDoc(doc(firestore, `UserLikes/${currentUser}`), {
+					upvotes: arrayRemove(time),
+					downvotes: arrayUnion(time),
+				});
+				setUpdatedScore(item.data().score - 1);
+			}
+		});
+	};
+
 	return (
 		<div className="comment">
 			<div className="comments-upvote">
-				<img src={up} alt="comment upvote" />
-				<img src={down} alt="comment downvote" />
+				<img src={up} alt="comment upvote" onClick={() => upvote()} />
+				<img src={down} alt="comment downvote" onClick={() => downvote()} />
 			</div>
 			<div className="comment-border">
 				<div className="comment-info">
 					<div className="comment-submitter">{name}</div>
-					<div className="comment-score">{score} points </div>
+					<div className="comment-score">{updatedScore} points </div>
 					<div className="comment-time">
 						{formatDistanceToNow(time, { includeSeconds: true })} ago
 					</div>

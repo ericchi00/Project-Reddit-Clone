80cea4605b3cad6c2fbd95426b324f2940e8f1d2
ericchi00 <ericchi00@gmail.com>
2022-04-27 13:20:53 -0700

    finished styling comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,23 +5,25 @@ import { formatDistanceToNow } from 'date-fns';
 
 const Comment = ({ name, score, text, time }) => {
 	return (
-		<div className='comment'>
+		<div className="comment">
 			<div className="comments-upvote">
 				<img src={up} alt="comment upvote" />
-				<p className="comment-score">{score}</p>
 				<img src={down} alt="comment downvote" />
 			</div>
-			<div className="comment-submitter">{name}</div>
-			<div className="comment-score">{score} points </div>
-			<div className="comment-time">
-				{formatDistanceToNow(time, { includeSeconds: true })} ago
+			<div className="comment-border">
+				<div className="comment-info">
+					<div className="comment-submitter">{name}</div>
+					<div className="comment-score">{score} points </div>
+					<div className="comment-time">
+						{formatDistanceToNow(time, { includeSeconds: true })} ago
+					</div>
+				</div>
+				<span className="comment-text">{text}</span>
 			</div>
-			<p className="comment-text">{text}</p>
 		</div>
 	);
 };
 
 export default Comment;
-
 
 // finish styling and check how often it reads firestore when refreshing

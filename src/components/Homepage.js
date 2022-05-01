29b94d4fccef8f5eeb9f6747bd6acdb4b6e23249
ericchi00 <ericchi00@gmail.com>
2022-04-30 17:44:46 -0700

    added in loading

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,57 +5,71 @@ import Post from './Post';
 const Homepage = ({ signedIn, currentUser }) => {
 	const [posts, setPosts] = useState([]);
 	const [removePost, setRemovePost] = useState(null);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-		document.title = 'Reddit Clone';
+		document.title = 'Reddit Clone Project';
 		grabAllPosts();
 		setRemovePost(false);
 	}, [removePost]);
 
 	const grabAllPosts = async () => {
-		// using set as the for loops add duplicate objects
-		let postsArr = new Set();
-		const db = getFirestore();
-		const querySnapshot = await getDocs(collection(db, 'Subreddit'));
-		querySnapshot.forEach(async (item) => {
-			const threads = await getDocs(
-				collection(db, 'Subreddit', `${item.data().name}`, 'posts')
-			);
-			threads.forEach((thread) => {
-				postsArr.add({
-					data: thread.data(),
-					name: item.data().name,
-					id: thread.id,
+		try {
+			// using set as the for loops add duplicate objects
+			let postsArr = [];
+			const db = getFirestore();
+			const querySnapshot = await getDocs(collection(db, 'Subreddit'));
+			// first for loop grabs each subreddit's name
+			querySnapshot.forEach(async (item) => {
+				let threads = await getDocs(
+					collection(db, 'Subreddit', `${item.data().name}`, 'posts')
+				);
+				// adds each post data to postsArr
+				threads.forEach((thread) => {
+					postsArr.push({
+						data: thread.data(),
+						name: item.data().name,
+						id: thread.id,
+					});
 				});
+				// sorts from high to low & removes duplicates
+				let newArr = [...new Set(postsArr)].sort((a, b) =>
+					b.data.score > a.data.score ? 1 : -1
+				);
+				setPosts(newArr);
+				setLoading(false);
 			});
-			// sorting by high score to low
-			let newArr = [...postsArr].sort((a, b) => b.data.score - a.data.score);
-			setPosts(newArr);
-		});
+		} catch (error) {
+			alert(error, 'Please try reloading page');
+		}
 	};
 
 	return (
 		<div className="subreddit">
 			<ul>
 				<div className="home">Homepage</div>
-				{posts.map((post, i) => {
-					return (
-						<Post
-							removePost={setRemovePost}
-							currentUser={currentUser}
-							signedIn={signedIn}
-							sub={post.name}
-							time={post.data.timestamp}
-							name={post.data.name}
-							score={post.data.score}
-							title={post.data.title}
-							text={post.data.text}
-							docID={post.id}
-							key={i}
-							index={i + 1}
-						/>
-					);
-				})}
+				{loading ? (
+					<div className="loading">homepage is loading data</div>
+				) : (
+					posts.map((post, i) => {
+						return (
+							<Post
+								removePost={setRemovePost}
+								currentUser={currentUser}
+								signedIn={signedIn}
+								sub={post.name}
+								time={post.data.timestamp}
+								name={post.data.name}
+								score={post.data.score}
+								title={post.data.title}
+								text={post.data.text}
+								docID={post.id}
+								key={post.data.timestamp}
+								index={i + 1}
+							/>
+						);
+					})
+				)}
 			</ul>
 		</div>
 	);
